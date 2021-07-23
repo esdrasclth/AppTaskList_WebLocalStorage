3a9327bf6e3d5ebf5d90784deb7ee2f882c82a64
Esdras Clother <esdras.clother@outlook.com>
2021-07-23 13:25:06 -0600

    adding form to project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import React, {useState} from 'react'
 import './App.css';
 import Header from './components/Header'
+import FormularioTareas from './components/FormularioTareas'
 
 const App = () => {
+
+  const [tareas, cambiarTareas] = useState(
+    [
+      {
+        id: 1,
+        texto: 'Lavar la ropa',
+        completada: false
+      },
+      {
+        id: 2,
+        texto: 'Grabar video',
+        completada: false
+      }
+    ]
+  )
+
+    console.log(tareas)
+
   return (
     <div className="contenedor">
       <Header/>
+      <FormularioTareas tareas={tareas} cambiarTareas={cambiarTareas}/>
     </div>
   );
 }
